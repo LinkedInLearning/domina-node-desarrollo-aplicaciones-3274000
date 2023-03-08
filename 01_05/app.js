bb1ac7945dc465cd3a7df41c894e503f2d89cb3a
Lincy Gonzalez Rojas <lincgr@gmail.com>
2023-03-08 09:28:45 +0100

    01_05 El uso de module.export en Node.js

---
+++ 01_05/app.js
@@ -0,0 +1,5 @@
+const operacion = require('./operaciones');
+
+const resultado = operacion.suma(10, 5);
+
+console.log('El resultado es ', resultado);
---
+++ 01_05/operaciones.js
@@ -0,0 +1,17 @@
+function suma(x, y) {
+    return x + y;
+}
+
+function resta(x, y) {
+    return x - y;
+}
+
+function multiplicacion(x, y) {
+    return x * y;
+}
+
+function division(x, y) {
+    return x / y;
+}
+
+module.exports = {suma, resta, multiplicacion, division};
