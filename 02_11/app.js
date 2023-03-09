4da59be7f42fec228b6d5d3a4788ce6cab95d1d8
Lincy Gonzalez Rojas <lincgr@gmail.com>
2023-03-09 11:32:50 +0100

    02_11 Filtrar un arreglo de objetos por una clave especifica

---
+++ 02_11/app.js
@@ -0,0 +1,11 @@
+const idPersonas = [1, 4, 5];
+const personas = [
+    { "id": 1, "nombre": "Augusto", "edad": 20 },
+    { "id": 2, "nombre": "Claudia", "edad": 15 },
+    { "id": 3, "nombre": "Lucio", "edad": 30 },
+    { "id": 4, "nombre": "Emilia", "edad": 18 },
+    { "id": 5, "nombre": "Valentina", "edad": 40 },
+    { "id": 6, "nombre": "Laura", "edad": 16 },
+    { "id": 7, "nombre": "Fabio", "edad": 17 },
+    { "id": 8, "nombre": "Octavio", "edad": 21 }
+];
