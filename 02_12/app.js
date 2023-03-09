f615835217cf52c0943ce0ec4b7e1d9be852fed8
Lincy Gonzalez Rojas <lincgr@gmail.com>
2023-03-09 11:59:27 +0100

    02_12 Ordenar arreglo de objetos por el valor de una clave

---
+++ 02_12/app.js
@@ -0,0 +1,12 @@
+let personas = [
+  {nombre: 'Sofía', apellido: 'Gutiérrez'},
+  {nombre: 'Alejandro', apellido: 'Hernández'},
+  {nombre: 'Valeria', apellido: 'García'},
+  {nombre: 'Diego', apellido: 'López'},
+  {nombre: 'Isabella', apellido: 'Ramírez'},
+  {nombre: 'Andrés', apellido: 'Pérez'},
+  {nombre: 'Ana María', apellido: 'Torres'},
+  {nombre: 'Juan Carlos', apellido: 'Sánchez'},
+  {nombre: 'Camila', apellido: 'Castro'},
+  {nombre: 'Mateo', apellido: 'Vargas'},
+];
