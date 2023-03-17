e4266fe0173d7ff4030cae4ed8252f2b0b153715
Lincy Gonzalez Rojas <lincgr@gmail.com>
2023-03-17 15:41:43 +0100

    02_02 ¿Cómo convertir el módulo commonjs al módulo es6 en Node.js?

---
+++ 02_02/app.js
@@ -0,0 +1,6 @@
+import { suma, resta} from './operaciones.js';
+
+const resultado = suma(10, 5);
+  
+console.log('El resultado de la suma es', resultado);
+console.log('El resultado de la resta es', resta(34,5));
---
+++ 02_02/operaciones.js
@@ -0,0 +1,9 @@
+function suma(x, y) {
+    return x + y;
+}
+
+function resta(x, y) {
+    return x - y;
+}
+
+export { suma, resta}
